aa65d3a0910f40480d2646cf65dc3b8fe142f4b0
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-01-26 13:13:22 +0200

    Project Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MediOra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
