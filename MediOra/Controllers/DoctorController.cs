9945c715c93931161cea12e483a90d819d55a1d5
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-22 18:54:40 +0200

    Added class all in doctor controller

--- MediOra/Controllers/DoctorController.cs
+++ MediOra/Controllers/DoctorController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediOra.Core.Contracts.Doctors;
+using MediOra.Core.Contracts.Specialties;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MediOra.Controllers
 {
     public class DoctorController : BaseController
     {
-        public IActionResult Index()
+        private readonly IDoctorService doctorService;
+        //private readonly ISpecialtyService specialtyService;
+
+        public DoctorController(IDoctorService _doctorService/*, ISpecialtyService _specialtyService*/)
         {
-            return View();
+            doctorService = _doctorService;
+           // specialtyService = _specialtyService;
+
+        }
+        public async Task<IActionResult> All(string searchTerm, int? specialtyId, string city)
+        {
+            var doctors = await doctorService.GetAllAsync();
+
+            return View(doctors);
         }
     }
 }
