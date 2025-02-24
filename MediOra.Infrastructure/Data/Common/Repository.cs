f03e39a7d1245ee718cbe8bb23a425f0a88755dc
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-24 06:02:05 +0200

    Added  SaveChangesAsync, GetByIdAsync, RemoveAsync, AddAsync

--- MediOra.Infrastructure/Data/Common/Repository.cs
+++ MediOra.Infrastructure/Data/Common/Repository.cs
@@ -26,5 +26,24 @@ namespace MediOra.Infrastructure.Data.Common
             return DbSet<T>()
                .AsNoTracking();
         }
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task RemoveAsync<T>(T entity) where T : class
+        {
+            DbSet<T>().Remove(entity);
+        }
     }
 }
