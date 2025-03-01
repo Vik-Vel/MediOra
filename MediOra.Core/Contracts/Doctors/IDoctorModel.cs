fc3f9ac449c4b2c588f207590b049253bf370452
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-03-01 18:54:44 +0200

    Created a model interface to more easily separate and manage logic when creating new views for a doctor.

---
+++ MediOra.Core/Contracts/Doctors/IDoctorModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MediOra.Core.Contracts.Doctors
+{
+    /// <summary>
+    /// We use IDoctorModel to define a common structure for doctor models that ensures consistency and reduces code duplication across different ViewModels and services.
+    /// </summary>
+    public interface IDoctorModel
+    {
+            string FirstName { get; set; }
+            string LastName { get; set; }
+            string PhoneNumber { get; set; }
+            string Address { get; set; }
+            string City { get; set; }
+            string Email { get; set; }
+            string? ImageUrl { get; set; }
+            string? Description { get; set; }
+            int SpecialtyId { get; set; }
+
+    }
+}
