53f82f6572265fc8913de21360e4ff42f66c699b
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-03-06 06:07:42 +0200

    Created IPatient Model for more clearly structure and implementing. Created IPatientService with needed actions for operations.

---
+++ MediOra.Core/Contracts/Patients/IPatientModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MediOra.Core.Contracts.Patients
+{
+    public interface IPatientModel
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string Address { get; set; }
+        string Email { get; set; }
+        DateTime DateOfBirth { get; set; }
+        string? ImageUrl { get; set; }
+    }
+}
--- MediOra.Core/Contracts/Patients/IPatientService.cs
+++ MediOra.Core/Contracts/Patients/IPatientService.cs
@@ -1,6 +1,18 @@
-﻿namespace MediOra.Core.Contracts.Patients
+﻿using MediOra.Core.Models.ViewModels.Patients;
+using MediOra.Core.Models.ViewModels.Specialities;
+
+namespace MediOra.Core.Contracts.Patients
 {
     public interface IPatientService
     {
+        Task<IEnumerable<PatientViewModel>> GetAllPatientsAsync();
+        Task<PatientViewModel> GetPatientByIdAsync(int id);
+        Task DeletePatientAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<PatientViewModel> DetailsAsync(int PatientId);
+        //Task AddPatientAsync(PatientCreateViewModel model);
+        //Task<PatientEditViewModel> EditPatientGetAsync(int PatientId);
+        //Task<int> EditPatientPostAsync(PatientEditViewModel editPatientForm);
+        //Task<IEnumerable<PatientViewModel>> GetFilteredPatientsAsync(string searchTerm, int? specialtyId);
     }
 }
