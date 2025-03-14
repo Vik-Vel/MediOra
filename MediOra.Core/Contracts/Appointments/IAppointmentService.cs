d70ccefd78f0d0b21537c1792c337fe853b05c4e
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-03-14 06:40:55 +0200

    Created IAppointmentService

--- MediOra.Core/Contracts/Appointments/IAppointmentService.cs
+++ MediOra.Core/Contracts/Appointments/IAppointmentService.cs
@@ -1,6 +1,20 @@
-﻿namespace MediOra.Core.Contracts.Appointments
+﻿using MediOra.Core.Models.ViewModels.Appointments;
+
+namespace MediOra.Core.Contracts.Appointments
 {
     public interface IAppointmentService
     {
+        Task<IEnumerable<AppointmentViewModel>> GetAllAsync();
+        Task<AppointmentViewModel> GetByIdAsync(int appointmentId);
+        Task CreateAsync(AppointmentViewModel model);
+        Task UpdateAsync(AppointmentViewModel model);
+        Task DeleteAsync(int appointmentId);
+        Task<bool> ExistsAsync();
+        Task<IEnumerable<AppointmentViewModel>> GetByDoctorAsync(int doctorId);
+        Task<IEnumerable<AppointmentViewModel>> GetByPatientAsync(int patientId);
+        Task<IEnumerable<AppointmentViewModel>> GetByDateRangeAsync(DateTime start, DateTime end);
+        Task<IEnumerable<(string Value, string Text)>> GetAvailableDoctors();
+        Task<IEnumerable<(string Value, string Text)>> GetAvailablePatients();
+        Task<IEnumerable<(string Value, string Text)>> GetAvailableTimeSlots(int doctorId, DateTime date);
     }
 }
