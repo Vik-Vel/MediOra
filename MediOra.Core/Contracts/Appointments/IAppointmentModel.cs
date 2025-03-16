919c17d8a1a1e9aeb7d8f35595a8bdc2b696aa49
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-03-16 22:21:03 +0200

    Created IAppoimentModel like base interfacex

---
+++ MediOra.Core/Contracts/Appointments/IAppointmentModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MediOra.Core.Contracts.Appointments
+{
+    /// <summary>
+    /// We use IAppointmentModel to define a common structure for appointment models that ensures 
+    /// consistency and reduces code duplication across different ViewModels and services.
+    /// </summary>
+    public interface IAppointmentModel
+    {
+        int PatientId { get; set; }
+        int DoctorId { get; set; }
+        DateTime ScheduledTime { get; set; }
+        int Duration { get; set; }
+        string Status { get; set; }
+        string Notes { get; set; }
+    }
+}
