537093841d2e2fce8df5a5ad3c9a7e8b7e28f373
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-03-17 21:26:38 +0200

    AppointmentListViewModel created

---
+++ MediOra.Core/Models/ViewModels/Appointments/AppointmentListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MediOra.Core.Contracts.Appointments;
+
+namespace MediOra.Core.Models.ViewModels.Appointments
+{
+    public class AppointmentListViewModel : IAppointmentModel
+    {
+        public int PatientId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int DoctorId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime ScheduledTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Duration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Notes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEnumerable<AppointmentViewModel> Appointments { get; set; } = Enumerable.Empty<AppointmentViewModel>();
+    }
+}
