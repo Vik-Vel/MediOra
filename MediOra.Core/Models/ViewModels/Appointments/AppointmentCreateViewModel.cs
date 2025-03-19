012e2fee49931bb9ccaa5c64bba794b89c404430
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-03-19 22:26:51 +0200

    AppointmentCreateViewModel created and implemented IAppointmentModel

---
+++ MediOra.Core/Models/ViewModels/Appointments/AppointmentCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MediOra.Core.Contracts.Appointments;
+
+namespace MediOra.Core.Models.ViewModels.Appointments
+{
+    public class AppointmentCreateViewModel : IAppointmentModel
+    {
+        public int PatientId { get ; set ; }
+        public int DoctorId { get; set; }
+        public DateTime ScheduledTime { get; set; }
+        public int Duration { get; set; }
+        public string Status { get ; set ; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+    }
+}
